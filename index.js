dc9854bc241fec3695f9b229fc8703b7aef92d88
xPoppa <88831116+xPoppa@users.noreply.github.com>
2022-05-12 17:34:42 +0200

    updated files to make it look better

--- index.js
+++ index.js
@@ -1,18 +1,11 @@
 // Initialize the different elements inputted number and text
 
 let mtofEl = document.getElementById("mtof-el")
-let ftomEl = document.getElementById("ftom-el")
-let ltogEl = document.getElementById("ltog-el")
 let gtolEl = document.getElementById("gtol-el")
-let ktopEl = document.getElementById("ktop-el")
 let ptokEl = document.getElementById("ptok-el")
-
-
-console.log("This is the number" + number)
-// let useNumber = number.textContent
-// console.log("should be the number value" + useNumber)
-let numberValue = 8
-useNumber = numberValue
+let lengthResult = mtofEl.textContent
+let volumeResult = gtolEl.textContent
+let weightResult = ptokEl.textContent
 
 
 function meterToFeet() {
@@ -20,12 +13,8 @@ function meterToFeet() {
     unit2 = " feet"
     converter = 3.28084
     var number = document.getElementById("number").value
-    console.log("This is the number" + number)
-    console.log("This is the number" + number)
     result = number * converter
-    console.log("this is the result " + result)
     roundedResult = Math.round(result*1000)/1000
-    console.log("this is the rounded result " + roundedResult)
     mtofEl.textContent = number + unit1 + roundedResult + unit2
 }
 
@@ -34,12 +23,9 @@ function feetToMeter() {
     unit2 = " meters"
     converter = 0.3048
     var number = document.getElementById("number").value
-    console.log("This is the number" + number)
     result = number * converter
-    console.log("this is the result " + result)
     roundedResult = Math.round(result*1000)/1000
-    console.log("this is the rounded result " + roundedResult)
-    ftomEl.textContent = number + unit1 + roundedResult + unit2
+    mtofEl.textContent += " | " + number + unit1 + roundedResult + unit2
     }
 
 
@@ -48,55 +34,43 @@ function feetToMeter() {
 function literToGallons() {
     unit1 = " liters = "
     unit2 = " gallons"
-    converter = 0.3048
+    converter = 0.2199692
     var number = document.getElementById("number").value
-    console.log("This is the number" + number)
     result = number * converter
-    console.log("this is the result " + result)
     roundedResult = Math.round(result*1000)/1000
-    console.log("this is the rounded result " + roundedResult)
-    ltogEl.textContent = number + unit1 + roundedResult + unit2
+    gtolEl.textContent = number + unit1 + roundedResult + unit2
 }
 
 function gallonsToLiters() {
     unit1 = " gallons = "
-    unit2 = " liters"    
-    converter = 0.3048
+    unit2 = " liters"
+    converter = 4.54609
     var number = document.getElementById("number").value
-    console.log("This is the number" + number)
     result = number * converter
-    console.log("this is the result " + result)
     roundedResult = Math.round(result*1000)/1000
-    console.log("this is the rounded result " + roundedResult)
-    gtolEl.textContent = number + unit1 + roundedResult + unit2
+    gtolEl.textContent += " | " + number + unit1 + roundedResult + unit2
 }
 
 /////////////////// Kilogras to Pounds and vice versa\\\\\\\\\\\\\\
 
 function kiloToPounds() {
     unit1 = " kilos = "
-    unit2 = " pounds"    
-    converter = 0.3048
+    unit2 = " pounds"
+    converter = 2.204623
     var number = document.getElementById("number").value
-    console.log("This is the number" + number)
     result = number * converter
-    console.log("this is the result " + result)
     roundedResult = Math.round(result*1000)/1000
-    console.log("this is the rounded result " + roundedResult)
-    ktopEl.textContent = number + unit1 + roundedResult + unit2
+    ptokEl.textContent = number + unit1 + roundedResult + unit2
 }
 
 function poundsToKilo() {
     unit1 = " pounds = "
-    unit2 = " kilos"    
-    converter = 0.3048
+    unit2 = " kilos"
+    converter = 0.4535924
     var number = document.getElementById("number").value
-    console.log("This is the number" + number)
     result = number * converter
-    console.log("this is the result " + result)
     roundedResult = Math.round(result*1000)/1000
-    console.log("this is the rounded result " + roundedResult)
-    ptokEl.textContent = number + unit1 + roundedResult + unit2
+    ptokEl.textContent += " | " + number + unit1 + roundedResult + unit2
 }
 
 function callAll() {
@@ -107,10 +81,3 @@ function callAll() {
     kiloToPounds()
     poundsToKilo()
 }
-
-// meterToFeet()
-// feetToMeter()
-// literToGallons()
-// gallonsToLiters()
-// kiloToPounds()
-// poundsToKilo()
